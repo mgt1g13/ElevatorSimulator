041900554351b635a7d5e0ccdec79c92a177d954
Matheus Trindade <matheustrindade@Air-de-Matheus.home>
2015-09-13 15:39:28 -0300

    Initial Commit

---
+++ ElevatorPthreads/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ElevatorPthreads
+//
+//  Created by Matheus Trindade on 13/09/15.
+//  Copyright (c) 2015 Matheus Trindade. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
